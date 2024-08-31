e2df4f19173965ae81914455e337e530bc60878a
harri taylor <harriworktaylor@gmail.com>
2024-08-31 00:48:09 +0000

    start game and

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,49 @@
-const startButton = document.getElementById('start-btn')
+const startBtn = document.getElementById('start-btn')
+const questionContainer = document.getElementById('question-box')
 
-startButton.addEventListener('click', start)
+startBtn.addEventListener('click', startGame)
 
-function start() {
+function startGame() {
     console.log("start")
+    startBtn.classList.add('hide')
+    questionContainer.classList.remove('hide')
+
 }
+
+function nextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
+
+let questions = [
+    {
+        question: "?",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: ,
+    },
+    {
+        question: "?",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: ,
+    },
+    {
+        question: "?",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: "",
+        answer: ,
+    },
+];
+
+const Correct_Point = 5;
+const Max_Question = 5;
