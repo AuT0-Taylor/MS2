c7bc8870e6c58f2f1a1dc74fd782ec6cdb708d5c
harri taylor <harriworktaylor@gmail.com>
2024-08-31 06:29:11 +0000

    rewrote functions code still no answers pop up

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,15 @@
-//constants
-const startBtn = document.getElementById('start-btn')
-const questionContainer = document.getElementById('question-box')
+/*Declaring Variables*/
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
 
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
 
-const Correct_Point = 5;
-const Max_Question = 5;
-
+const CORRECT_BONUS = 5;
+const MAX_QUESTIONS = 3;
 
 /* questions */
 let questions = [
@@ -18,39 +22,60 @@ let questions = [
         answer: 1,
     },
     {
-        question: "?",
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        choice4: "",
-        answer: 1,
+        question: "What type is effective against ground?",
+        choice1: "electric",
+        choice2: "steel",
+        choice3: "fairy",
+        choice4: "water",
+        answer: 4,
     },
     {
-        question: "?",
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        choice4: "",
-        answer: 1,
+        question: "What type is super effective against dragon?",
+        choice1: "dragon",
+        choice2: "flying",
+        choice3: "fairy",
+        choice4: "poison",
+        answer: 3,
     },
 ];
 
-/* starts game function
-- hide start btn
-- intiates quiz with array question */
-function startGame() {
+/*Functions*/
+
+startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuesions = [...questions];
-    nextQuestion();
-    startBtn.classList.add('hide')
-    questionContainer.classList.remove('hide')
-}
+    getNewQuestion();
+};
 
-function nextQuestion() {
+getNewQuestion = () => {
+    /*finished quiz goes to end html*/
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        return window.location.assign('/end.html');
+    }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
+    question.innerText = currentQuestion.question;
 
-}
+    choices.forEach( (choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    } );
 
-function selectAnswer() {
+    availableQuesions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
 
-}
+choices.forEach( (choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+        getNewQuestion();
+    } );
+} );
+
+startGame();
